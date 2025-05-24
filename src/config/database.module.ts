e74d81148761b01515d3ef164833ea5a4a6e1103
AshenSachinthana <dasachinthana@gmail.com>
2025-05-24 20:19:14 +0530

    Add Sequelize and Config modules for database integration

---
+++ src/config/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+      }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: true, 
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [SequelizeModule],
+})
+export class DatabaseModule {}
