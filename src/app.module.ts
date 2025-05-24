72e54e1e0a71486a82fbbe8f1116bd8c16fe9760
AshenSachinthana <dasachinthana@gmail.com>
2025-05-24 22:36:50 +0530

    feat: add authentication and user modules, enable CORS, and update dependencies
    - Added DatabaseModule, AuthModule, and UsersModule to the main application module.
    - Enabled CORS in the main application bootstrap function.
    - Updated package.json to include necessary dependencies for authentication and user management, including bcrypt, passport, and related types.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
